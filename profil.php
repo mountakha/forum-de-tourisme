5242218bd9fe0a03dcf752a8c7d8919db41650ed
mountakha <mountakha.djigo@ynov.com>
2018-04-16 13:11:36 +0200

    Create profil.php

---
+++ profil.php
@@ -0,0 +1,37 @@
+<style>
+
+</style>
+
+
+<?php include 'header.php'; ?>
+<div class="ok">
+
+    <h2 style="text-align:center">User Profile Card</h2>
+
+<div class="card">
+  <img src="./image/pri.png" alt="John">
+  <h1>Pseudo:</h1>
+  <p class="title"><?php  ?></p>
+  <p><?php  ?></p>
+  <div style="margin: 24px 0;">
+    <a href="#"><i class="fa fa-dribbble"></i></a> 
+    <a href="#"><i class="fa fa-twitter"></i></a>  
+    <a href="#"><i class="fa fa-linkedin"></i></a>  
+    <a href="#"><i class="fa fa-facebook"></i></a> 
+ </div>
+ <p><button>Contact</button></p>
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
