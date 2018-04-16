4dd7991156b111f515411cfa7bd9caad52167ff4
mountakha <mountakha.djigo@ynov.com>
2018-04-16 10:27:53 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,54 +1,50 @@
 
+
+
+
 <?php include 'header.php'; ?>
 
 <div class="container">
-  <form action="action_page.php">
+  <form action="inscription.validation.php">
     <div class="row">
       <div class="col-25">
-        <label for="fname">First Name</label>
+        <label for="fname">Pseudo</label>
       </div>
       <div class="col-75">
-        <input type="text" id="fname" name="firstname" placeholder="Your name..">
+        <input type="text" id="pseudo" name="firstname" placeholder="Your name..">
       </div>
     </div>
     <div class="row">
       <div class="col-25">
-        <label for="lname">Last Name</label>
+        <label for="lname">mail</label>
       </div>
       <div class="col-75">
-        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+        <input type="text" id="lname" name="mail" placeholder="Your last name..">
       </div>
     </div>
 
 
     <div class="row">
       <div class="col-25">
-        <label for="lname">Last Name</label>
+        <label for="lname">Password</label>
       </div>
       <div class="col-75">
-        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+        <input type="text" id="lname" name="password" placeholder="Your last name..">
       </div>
     </div>
 
     <div class="row">
       <div class="col-25">
-        <label for="lname">Last Name</label>
+        <label for="lname">Confirm</label>
       </div>
       <div class="col-75">
-        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+        <input type="text" id="confirme" name="lastname" placeholder="Your last name..">
       </div>
     </div>
     
+    
     <div class="row">
-      <div class="col-25">
-        <label for="subject">Subject</label>
-      </div>
-      <div class="col-75">
-        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-      </div>
-    </div>
-    <div class="row">
-      <input type="submit" value="Submit">
+    <button type="button" class="btn" id="submit-btn" style="background-color:#242831;color:#ce813d;border:solid 1px #ce813d">POST</button>
     </div>
   </form>
 </div> 
