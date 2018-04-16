7270c57afe29012b1908046d7da03e7323dff7c1
mountakha <mountakha.djigo@ynov.com>
2018-04-16 10:29:05 +0200

    Create Topic.php

---
+++ Topic.php
@@ -0,0 +1,41 @@
+
+<?php include 'header.php'; ?>
+
+
+<h1>Topic France</h1>
+
+
+<button class="btn" style="background:#242831;border:2px solid orange;color:orange" type="submit">Reply</button>
+
+
+</br>
+
+</br>
+
+</br>
+
+
+<div class="pagination">
+  <a href="#">&laquo;</a>
+  <a href="#">1</a>
+  <a href="#" class="active">2</a>
+  <a href="#">3</a>
+  <a href="#">4</a>
+  <a href="#">5</a>
+  <a href="#">6</a>
+  <a href="#">&raquo;</a>
+</div>
+
+
+<ul style="color:;background:#242831;">
+  <li><a class="ol" href="#home" ><img src="./image/logo.png" alt="" style="width:90px;height:50px;"></a></li>
+  <li><a href="#news"<p>Administrator</p></a></li>
+
+</ul>
+
+
+<p>lieu:</p>
+
+
+
+<?php include 'footer.php'; ?>
